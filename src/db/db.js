8871f23034fe238d83ac728988f12eeae33fadd4
Keshav Saini <sainikeshav2004@gmail.com>
2026-03-14 12:06:08 +0530

    fix mongoose collection

--- src/db/db.js
+++ src/db/db.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 function connectToDb(){
     mongoose.connect(process.env.MONGODB_URL)
-    .then(()=>{
-        console.log("Connected To DB")
-    })
+.then(()=>{
+    console.log("MongoDB connected:", mongoose.connection.name)
+})
+.catch((err)=>{
+    console.log(err)
+})
 }
 
 module.exports=connectToDb
--- src/model/songs.model.js
+++ src/model/songs.model.js
@@ -1,11 +1,12 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const songSchema=new mongoose.Schema({
-    title:String,
-    artist:String,
-    audioFile:String,
-    mood:String
+const songSchema = new mongoose.Schema({
+  title: String,
+  artist: String,
+  audioFile: String,
+  mood: String
 });
 
-const songModel=mongoose.model("all-songs",songSchema);
-module.exports=songModel;
+const songModel = mongoose.model("Song", songSchema, "all-songs");
+
+module.exports = songModel;
